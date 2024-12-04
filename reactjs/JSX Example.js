0488c790ad9a02e9796e4f16a996da2741d50d43
vardapattanshetty <146344101+vardapattanshetty@users.noreply.github.com>
2024-12-04 22:42:18 +0530

    Create JSX Example.js

---
+++ reactjs/JSX Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  const name = 'React Developer';
+  return (
+    <div>
+      <h1>Welcome to React, {name}!</h1>
+      <p>This is a simple example of JSX syntax.</p>
+    </div>
+  );
+}
+
+export default App;
